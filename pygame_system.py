e28f85af59006fb0d70ab4cb77db4fb9aa45ef70
c0b23120 <c0b23120bb@edu.teu.ac.jp>
2024-04-16 17:09:38 +0900

    FPS retake

--- pygame_system.py
+++ pygame_system.py
@@ -10,11 +10,16 @@ def main():
     screen = pg.display.set_mode((800, 600))
     clock = pg.time.Clock()
     font = pg.font.Font(None, 80)
+    img = pg.image.load("fig/3.png")
 
     enn = pg.Surface((20, 20))
     pg.draw.circle(enn, (255, 0, 0), (10, 10), 10)
+
     enn.set_colorkey((0, 0, 0))
 
+    screen.blit(img, [300, 200])
+
+    
     tmr = 0
     while True:
         for event in pg.event.get():
