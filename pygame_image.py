1ed299a3fd056276c30616dec4a8b315f40408e2
c0b23120 <c0b23120bb@edu.teu.ac.jp>
2024-04-16 17:29:49 +0900

    練習5

--- pygame_image.py
+++ pygame_image.py
@@ -13,7 +13,7 @@ def main():
     bg_img = pg.image.load("fig/pg_bg.jpg")
     bg_img2 = pg.transform.flip(bg_img,True,False)
     kk_img = pg.image.load("fig/3.png")
-    kk_img2 = pg.transform.flip(kk_img,True,False)
+    kk_img = pg.transform.flip(kk_img,True,False)
 
     kk_rct = kk_img2.get_rect()
     kk_rct.center=300,200
